2b5052f9f74c2fe86b89d76c1fdaed9b1f0c8768
DreamerArise <sokhnadiarradiop@esp.sn>
2024-04-08 16:38:41 +0000

    Success!# Please enter the commit message for your changes. Lines starting

--- app-gestion-enseignants/src/app/app.component.spec.ts
+++ app-gestion-enseignants/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'gestion-enseignants' title`, () => {
+  it(`should have the 'app-gestion-enseignants' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gestion-enseignants');
+    expect(app.title).toEqual('app-gestion-enseignants');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gestion-enseignants');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, app-gestion-enseignants');
   });
 });
--- app-gestion-enseignants/src/app/app.component.ts
+++ app-gestion-enseignants/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'gestion-enseignants';
+  title = 'app-gestion-enseignants';
 }
