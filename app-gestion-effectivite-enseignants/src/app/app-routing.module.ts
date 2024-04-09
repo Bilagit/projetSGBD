cef6555cb465fa92b6af7e47fd77ab46e49f85ba
DreamerArise <sokhnadiarradiop@esp.sn>
2024-04-09 14:24:55 +0000

    Yallah bahna

--- app-gestion-effectivite-enseignants/src/app/app-routing.module.ts
+++ app-gestion-effectivite-enseignants/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './_utils/error/error.component';
 import { AuthGuard } from './_helpers/auth.guard';
+import { ProfilEtudiantComponent } from './profil/profil-etudiant/profil-etudiant.component';
+import { ProfilEnseignantComponent } from './profil/profil-enseignant/profil-enseignant.component';
+
 
 const routes: Routes = [
   {
@@ -15,6 +18,9 @@ const routes: Routes = [
   {path: 'auth', loadChildren: () => import('./auth/auth.module')
   .then(m => m.AuthModule)
   },
+
+  { path: 'profil-etudiant', component: ProfilEtudiantComponent },
+  { path: 'profil-enseignant', component: ProfilEnseignantComponent },
 
   {path: '**', component: ErrorComponent}
  
--- app-gestion-effectivite-enseignants/src/app/auth/login/login.component.ts
+++ app-gestion-effectivite-enseignants/src/app/auth/login/login.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form : any = {
+    email : null,
+    password : null
+  }
+
+  constructor() {}
+  ngOnInit(): void {
+      
+  }
+  onSubmit() {
+    console.log(this.form)
+  }
+}*/
+
+// login.component.ts
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  selectedProfile: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    // Validation des informations de connexion
+    if (this.isValidEmail(this.email) && this.password === 'admin' && this.selectedProfile !== '') {
+      // Redirection vers le profil approprié
+      if (this.selectedProfile === 'étudiant') {
+        this.router.navigate(['/profil-etudiant']);
+      } else if (this.selectedProfile === 'enseignant') {
+        this.router.navigate(['/profil-enseignant']);
+      }
+    } else {
+      // Affichage d'un message d'erreur si les informations de connexion sont incorrectes
+      alert('Adresse email, mot de passe ou profil incorrect(s). Veuillez réessayer.');
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@esp\.sn$/;
+    return emailRegex.test(email);
+  }
 }
+
