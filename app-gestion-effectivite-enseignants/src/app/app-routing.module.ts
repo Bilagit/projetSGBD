3ed3a5256d2794c9933119e66c69141a4fcc2a2e
DreamerArise <sokhnadiarradiop@esp.sn>
2024-04-09 02:22:24 +0000

    yallah bahna

--- app-gestion-effectivite-enseignants/src/app/app-routing.module.ts
+++ app-gestion-effectivite-enseignants/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './_utils/error/error.component';
+import { AuthGuard } from './_helpers/auth.guard';
 
 const routes: Routes = [
   {
-    path: "", loadChildren: () => import('./pulic/pulic.module')
+    path: '', loadChildren: () => import('./pulic/pulic.module')
     .then(m => m.PulicModule)
-  }
+  },
+  {path: 'admin', loadChildren: () => import('./admin/admin.module')
+    .then(m => m.AdminModule), canActivate:[AuthGuard] 
+  },
+
+  {path: 'auth', loadChildren: () => import('./auth/auth.module')
+  .then(m => m.AuthModule)
+  },
+
+  {path: '**', component: ErrorComponent}
+ 
 ];
 
 @NgModule({
